6d260b726ed2542d30295dc9126c43d534b239ce
Cai <13110818005@qq.com>
2026-02-12 22:06:29 +0800

    feat: update for Terraria 1.4.5

--- TShockFishShop/Shop/ForgeItem.cs
+++ TShockFishShop/Shop/ForgeItem.cs
@@ -18,7 +18,7 @@ public class ForgeItem : ShopItem
         if (msg != "") return msg;
 
         Item forgeItem = op.TPlayer.inventory[0];
-        int id = forgeItem.netID;
+        int id = forgeItem.type;
         if (id == 0) return "需将要重铸的物品，放在背包的第1格！";
         if (!Prefix.CanHavePrefixes(forgeItem)) return $"[i:{id}]不能重铸";
 
@@ -65,7 +65,7 @@ public class ForgeItem : ShopItem
     public override void ProvideGoods()
     {
         Item forgeItem = op.TPlayer.inventory[0];
-        int id = forgeItem.netID;
+        int id = forgeItem.type;
 
         Item item = new();
         item.SetDefaults(id);
